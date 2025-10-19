637816d8e981b160549b52f0e11e1e8d3b96aa19
ArgenisValdez <argenisvalde80@gmail.com>
2025-10-19 13:43:55 -0400

    sencillo

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,9 @@
 window.addEventListener("load", function() {
   const loader = document.getElementById("loader");
   const main = document.querySelector("main");
-
-  // Activar animación de desaparición
+    
   loader.classList.add("hide");
 
-  // Luego de la transición, ocultar el loader y mostrar el contenido
   setTimeout(() => {
   loader.style.display = "none";
   main.style.display = "block";
